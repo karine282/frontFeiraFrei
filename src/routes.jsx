fc6b4266bde011e4b51e8af4803c4dc067da967d
vnzz477 <ra54865444807@acaonsfatima.org.br>
2025-09-19 18:18:33 -0300

    Subindo versão com erros para ajuste

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,16 +2,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./pages/app/App.jsx";
 import Sobre from "./pages/sobre/Sobre.jsx";
 import Login from "./pages/login/Login.jsx";
-import Cadastro from "./pages/Cadastro/Cadastro.jsx";
-import Tecnico from "./pages/Cursos/Tec.jsx";
-import CV from "./pages/Cursos/Cv.jsx";
-import Ingles from "./pages/Cursos/Ingles.jsx";
-import Atomocao from "./pages/Cursos/Robo.jsx";
-import Eletro from "./pages/Cursos/Ele.jsx";
-import Informatica from "./pages/Cursos/Info.jsx"
+import Cadastro from "./pages/cadastro/Cadastro.jsx";
+import Tecnico from "./pages/cursos/Tec.jsx";
+import CV from "./pages/cursos/Cv.jsx";
+import Ingles from "./pages/cursos/Ingles.jsx";
+import Automacao from "./pages/cursos/Robo.jsx";
+import Eletro from "./pages/cursos/Ele.jsx";
+import Informatica from "./pages/cursos/Info.jsx";
 import BuscarVisitante from "./pages/buscar/Buscar.jsx";
 import VincularUsuario from "./pages/buscar/VincularUsuario.jsx";
-
 
 export default function Navegacao() {
   return (
@@ -24,9 +23,9 @@ export default function Navegacao() {
         <Route path="/tec" element={<Tecnico />} />
         <Route path="/cv" element={<CV />} />
         <Route path="/ingles" element={<Ingles />} />
-        <Route path="/automacao" element={<Atomocao />} />
+        <Route path="/automacao" element={<Automacao />} />
         <Route path="/eletro" element={<Eletro />} />
-        <Route path="/info" element={<Informatica/>} />
+        <Route path="/info" element={<Informatica />} />
         <Route path="/buscar" element={<BuscarVisitante />} />
         <Route path="/vincular" element={<VincularUsuario />} />
       </Routes>
